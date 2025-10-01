5e2efef16a95ce1537331c726372d18fb318f2f5
CarlosBueloni <carlosbueloni@gmail.com>
2025-10-01 10:16:51 -0300

    Add schema to get_file_content

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,6 @@
 import os
 import config
+from google.genai import types
 
 def get_file_content(working_directory,file_path):
     abs_working_dir = os.path.abspath(working_directory)
@@ -17,3 +18,18 @@ def get_file_content(working_directory,file_path):
             return file_content_string
     except Exception as e:
         return f"Error reading file: {e}"
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="List content from the specified file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file path to list content from, relative to the working directory."
+            )
+        }
+    )
+    
+)
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -25,7 +25,7 @@ def get_files_info(working_directory, directory="."):
 
 schema_get_files_info = types.FunctionDeclaration(
     name="get_files_info",
-    description="Liosts files in the specified directory along with their sizes, constrained to the working directory.",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
     parameters=types.Schema(
         type=types.Type.OBJECT,
         properties={
