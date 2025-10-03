246e8fe76f69e90d8e6072b77c58cdcd782cef21
CarlosBueloni <carlosbueloni@gmail.com>
2025-10-02 21:12:19 -0300

    start implement calling functions

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from functions import get_files_info
+from functions import run_python_file
 from prompts import system_prompt
 
 from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
+from functions.get_file_content import get_file_content, schema_get_file_content
 from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from functions.write_file import schema_write_file, write_file
 
 def main():
     load_dotenv()
@@ -66,6 +68,45 @@ def generate_content(client, messages, verbose):
             print(f"Calling function: {function_call_part.name}({function_call_part.args})")
     else:
         print(response)
+
+def call_function(function_call_part, verbose=False):
+    function_name = function_call_part.name
+    function_args = function_call_part.args
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+
+    functions = {
+        "get_file_content": get_file_content,
+        "get_files_info": get_files_info,
+        "run_python_file": run_python_file,
+        "write_file": write_file,
+    }
+
+    if function_call_part.name not in functions:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+
+    function_result = functions[function_name]("./calculator", **function_args)
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
+
+
 
 if __name__ == "__main__":
     main()
