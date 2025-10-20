08904abb32035b24fd6f6eb6bca6dc2729b5cbc4
CarlosBueloni <carlosbueloni@gmail.com>
2025-10-20 16:36:07 -0300

    Add system prompt config to the ai agent

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from functions.run_python import run_python_file
 
 def test():
-    result = run_python_file("calculator", "main.py")
+    result = run_python_file("calculator", "main.py", "sdklfj")
     print(result)
     result = run_python_file("calculator", "tests.py")
     print(result)
